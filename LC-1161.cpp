abe80a60133221c324070cfd405819ea24d0d33c
HNM <hemantmarathe3108@gmail.com>
2026-01-10 10:14:51 +0530

    CHanges made to run code on vs code code

--- LC-1161.cpp
+++ LC-1161.cpp
@@ -1,21 +1,22 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+#include <queue>
+#include<bits/stdc++.h>
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
 class Solution {
 public:
     int maxLevelSum(TreeNode* root) {
         if(root ==nullptr){
             return 0;
         }
-        queue<TreeNode *>  q;
+        queue<TreeNode*> q;
         q.push(root);
 
         int maxsum=INT_MIN;
