981ec1449d00140d90c2b3060715b05eb5bff7a9
HNM <hemantmarathe3108@gmail.com>
2026-01-10 10:05:21 +0530

    Maximum Level Sum of Binary Tree

---
+++ LC-1161.cpp
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxLevelSum(TreeNode* root) {
+        if(root ==nullptr){
+            return 0;
+        }
+        queue<TreeNode *>  q;
+        q.push(root);
+
+        int maxsum=INT_MIN;
+        int maxlevel=1;
+        int level =1;
+        while(!q.empty()){
+            int levelsum=0;
+            int levelsize=q.size();
+            for(int i=0;i<levelsize;i++){
+                TreeNode * node = q.front();
+                q.pop();
+                levelsum += node->val;
+                if(node->left){
+                    q.push(node->left);
+                }
+                if(node->right){
+                    q.push(node->right);
+                }
+            }
+            if(levelsum>maxsum){
+                maxsum = levelsum;
+                maxlevel=level;
+            }
+            level ++;
+
+        }
+
+        return maxlevel;
+    }
+};
