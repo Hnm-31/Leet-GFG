8e1e6162f1249d8793b327fb3de7acfda6263ede
Hemant h. <161869380+Hnm-31@users.noreply.github.com>
2026-01-18 11:57:19 +0530

    Time: 1 ms (37.81%), Space: 95 MB (79.55%) - LeetHub

---
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+     int n1 = nums1.size();
+     int n2=nums2.size();
+     int n = n1+n2;
+     int ind1 = n/2 -1;
+     int ind2 = n/2;
+     int ind1el=-1;
+     int ind2el = -1;
+     int i=0,j=0;
+     int cnt=0;
+     while(i<n1 && j<n2 && (ind1el == -1 || ind2el ==-1)  ){
+        if(nums1[i]<nums2[j]){
+            
+            if(cnt == ind1) ind1el = nums1[i];
+            if(cnt ==ind2) ind2el = nums1[i];
+            cnt ++;
+            i++;
+
+        }
+        else{
+            
+            if(cnt == ind1) ind1el = nums2[j];
+            if(cnt ==ind2) ind2el = nums2[j];
+            cnt ++;
+            j++;
+        }
+     }
+
+     while(i<n1 && (ind1el == -1 || ind2el ==-1)  ){
+        
+         if(cnt == ind1) ind1el = nums1[i];
+            if(cnt ==ind2) ind2el = nums1[i];
+            cnt ++;
+            i++;
+     }
+
+     
+     while(j<n2 && (ind1el == -1 || ind2el ==-1)  ){
+         
+            if(cnt == ind1) ind1el = nums2[j];
+            if(cnt ==ind2) ind2el = nums2[j];
+            cnt ++;
+            j++;
+     }
+        if(n %2 !=0) return ind2el;
+        return (double) (ind1el + ind2el)/2;
+
+        
+
+    }
+};
